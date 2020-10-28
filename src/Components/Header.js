f29a4ea0eeff899b3d287b081db9aab009a7619a
Antoine <ant.g@hotmail.be>
2020-10-28 11:30:58 +0100

    commit 2

---
+++ src/Components/Header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Carousel } from 'rsuite';
+import { render } from "react-dom";
+
+const instance = (
+    <Carousel autoplay className="custom-slider">
+        <img
+            src="https://via.placeholder.com/600x250/8f8e94/FFFFFF?text=1"
+            height="250"
+        />
+        <img
+            src="https://via.placeholder.com/600x250/8f8e94/FFFFFF?text=2"
+            height="250"
+        />
+        <img
+            src="https://via.placeholder.com/600x250/8f8e94/FFFFFF?text=3"
+            height="250"
+        />
+        <img
+            src="https://via.placeholder.com/600x250/8f8e94/FFFFFF?text=4"
+            height="250"
+        />
+        <img
+            src="https://via.placeholder.com/600x250/8f8e94/FFFFFF?text=5"
+            height="250"
+        />
+    </Carousel>
+);
+
+
+
+class Header extends React.Component {
+    render() {
+        return(
+            <div>
+                {instance}
+            </div>
+        )
+    }
+}
+
+
+  
+  
+export default Header;
