bc6a6031b3d83142cac9cd8f3a521ccaeebe8170
Antoine <ant.g@hotmail.be>
2020-11-06 09:48:12 +0100

    filter

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ function App() {
   return (
     <div className="App">
       <NavbarMenu/>
-      <l><Link to='/'>Sneakers</Link></l>
-      <l><Link to='/FuturDrops'>FutureDrops</Link></l>
+      <l><Link to='/'>sneakers</Link></l>
+      <l><Link to='/future-drops'>FutureDrops</Link></l>
       <Switch>
         <Route exact path='/' component={Sneakers}/>
-        <Route path='/FutureDrops' component={FutureDrops}/>
+        <Route path='/future-drops' component={FutureDrops}/>
       </Switch>
-    </div>
+    </div> 
   );
 }
 
