47b01cb688c6c5ff764a9a5c17ad1040e980d1e3
Antoine <ant.g@hotmail.be>
2020-11-06 10:12:13 +0100

    m

--- src/App.js
+++ src/App.js
@@ -10,19 +10,11 @@ function App() {
   return (
     <div className="App">
       <NavbarMenu/>
-<<<<<<< HEAD
-      <l><Link to='/'>Sneakers</Link></l>
-      <l><Link to='/futuredrops'>FutureDrops</Link></l>
-      <Switch>
-        <Route exact path='/' component={Sneakers}/>
-        <Route path='/futuredrops' component={FutureDrops}/>
-=======
       <l><Link to='/'>sneakers</Link></l>
       <l><Link to='/future-drops'>FutureDrops</Link></l>
       <Switch>
         <Route exact path='/' component={Sneakers}/>
         <Route path='/future-drops' component={FutureDrops}/>
->>>>>>> bc6a6031b3d83142cac9cd8f3a521ccaeebe8170
       </Switch>
     </div> 
   );
