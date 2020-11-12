ef02e5a2621d1ed6ea8858be65a1e861486045f9
Antoine <ant.g@hotmail.be>
2020-11-12 14:26:04 +0100

    wish list

--- src/Components/FutureDrops.js
+++ src/Components/FutureDrops.js
@@ -14,25 +14,25 @@ const FutureDrops = (props) => {
         <Accordion>
                 <Card>
                     <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                        <img className={styles.imgFuturDrops} src="https://sneakernews.com/wp-content/uploads/2020/09/adidas-yeezy-380-onyx-release-date-6.jpg?w=1140"/>
+                        <img className={styles.imgFuturDrops} src="https://sneakernews.com/wp-content/uploads/2020/09/adidas-yeezy-380-onyx-release-date-6.jpg?w=1140" alt="" />
                     
                         <article className={styles.articleLayout}>
                             <h2 className={styles.textStyle}>adidas Yeezy Boost 380 Onyx</h2>
                             <h4 className={styles.textStyle}>C’est désormais officiel, la très attendue adidas Yeezy Boost 380 Onyx fera ses débuts à l’occasion du Black Friday 2020.</h4>
                             <p className={styles.textStyle}> 
-                    Cette déclinaison inédite, que l’on pourrait presque qualifier de Triple Black, affiche une empeigne Primeknit gris et noir. 
-                    La tige est également agrémentée d’un imprimé abstrait proche du snakeskin, de perforations sur le quarter panel, ainsi que de lacets bicolores. 
-                    La adidas Yeezy Boost 380 Onyx sortira le 27 novembre 2020 sur Adidas.fr et chez certains revendeurs, au tarif habituel de 230€. Bon cop!</p>
+                            Cette déclinaison inédite, que l’on pourrait presque qualifier de Triple Black, affiche une empeigne Primeknit gris et noir. 
+                            La tige est également agrémentée d’un imprimé abstrait proche du snakeskin, de perforations sur le quarter panel, ainsi que de lacets bicolores. 
+                            La adidas Yeezy Boost 380 Onyx sortira le 27 novembre 2020 sur Adidas.fr et chez certains revendeurs, au tarif habituel de 230€. Bon cop!</p>
                             <Button variant="dark" href="https://www.lesitedelasneaker.com/release/adidas-yeezy-boost-380-onyx/">Link to article</Button>{' '}
                         </article>
                         </Accordion.Toggle>
                     <Accordion.Collapse eventKey="0">
                     <Card.Body>
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
                     </Card.Body>
                     </Accordion.Collapse>
                 </Card>
@@ -42,7 +42,7 @@ const FutureDrops = (props) => {
         <Accordion>
                 <Card>
                     <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                        <img className={styles.imgFuturDrops} src="https://sneakernews.com/wp-content/uploads/2020/10/FRAME-SKATE-Nike-SB-Dunk-Low-Habibi-6.jpg?w=1140"/>
+                        <img className={styles.imgFuturDrops} src="https://sneakernews.com/wp-content/uploads/2020/10/FRAME-SKATE-Nike-SB-Dunk-Low-Habibi-6.jpg?w=1140" alt="" />
                     
                         <article className={styles.articleLayout}>
                             <h2 className={styles.textStyle}>Frame Skate x Nike SB Dunk Low Habibi</h2>
@@ -56,11 +56,11 @@ const FutureDrops = (props) => {
                         </Accordion.Toggle>
                     <Accordion.Collapse eventKey="0">
                     <Card.Body>
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
                     </Card.Body>
                     </Accordion.Collapse>
                 </Card>
@@ -70,7 +70,7 @@ const FutureDrops = (props) => {
         <Accordion>
                 <Card>
                     <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                        <img className={styles.imgFuturDrops} src="https://fastsole.co.uk/wp-content/uploads/2020/10/The-Yeezy-Quantum-‘Frozen-Blue-Featuring-A-Glow-In-The-Dark-Upper-f-1280x720.jpg"/>
+                        <img className={styles.imgFuturDrops} src="https://fastsole.co.uk/wp-content/uploads/2020/10/The-Yeezy-Quantum-‘Frozen-Blue-Featuring-A-Glow-In-The-Dark-Upper-f-1280x720.jpg" alt="" />
                     
                         <article className={styles.articleLayout}>
                             <h2 className={styles.textStyle}>adidas Yeezy Quantum Frozen Blue</h2>
@@ -84,11 +84,11 @@ const FutureDrops = (props) => {
                         </Accordion.Toggle>
                     <Accordion.Collapse eventKey="0">
                     <Card.Body>
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
                     </Card.Body>
                     </Accordion.Collapse>
                 </Card>
@@ -98,7 +98,7 @@ const FutureDrops = (props) => {
         <Accordion>
                 <Card>
                     <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                        <img className={styles.imgFuturDrops} src="https://i2.wp.com/deadlaced.com/wp-content/uploads/2020/05/5a1ade88-1099bb12-32d1-4300-8ad9-12f1ca6217da.png?fit=750%2C546&ssl=1"/>
+                        <img className={styles.imgFuturDrops} src="https://i2.wp.com/deadlaced.com/wp-content/uploads/2020/05/5a1ade88-1099bb12-32d1-4300-8ad9-12f1ca6217da.png?fit=750%2C546&ssl=1" alt="" />
                     
                         <article className={styles.articleLayout}>
                             <h2 className={styles.textStyle}>Air Jordan 1 Retro High OG WMNS "Silver Toe"</h2>
@@ -111,11 +111,11 @@ const FutureDrops = (props) => {
                         </Accordion.Toggle>
                     <Accordion.Collapse eventKey="0">
                     <Card.Body>
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
                     </Card.Body>
                     </Accordion.Collapse>
                 </Card>
@@ -125,7 +125,7 @@ const FutureDrops = (props) => {
         <Accordion>
                 <Card>
                     <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                        <img className={styles.imgFuturDrops} src="https://houseofheat.co/app/uploads/2019/08/air-jordan-1-retro-high-og-unc-555088-140-sail-obsidian-university-blue-release-date-info-8-1200x749.jpg"/>
+                        <img className={styles.imgFuturDrops} src="https://houseofheat.co/app/uploads/2019/08/air-jordan-1-retro-high-og-unc-555088-140-sail-obsidian-university-blue-release-date-info-8-1200x749.jpg" alt="" />
                     
                         <article className={styles.articleLayout}>
                             <h2 className={styles.textStyle}>Air Jordan 1 High OG University Blue</h2>
@@ -139,11 +139,11 @@ const FutureDrops = (props) => {
                         </Accordion.Toggle>
                     <Accordion.Collapse eventKey="0">
                     <Card.Body>
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
-                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
+                        <img className={styles.imgInsta} src="https://via.placeholder.com/200" alt="" />
                     </Card.Body>
                     </Accordion.Collapse>
                 </Card>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,28 +9,23 @@ const head = (
     <Carousel autoplay className="custom-slider" className={styles.Carousel} >
         <img className={styles.imageCarousel}
             src="https://images.unsplash.com/photo-1488704906310-183eeda75d51?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-            // src ="https://stockx-360.imgix.net//adidas-Yeezy-Boost-350-V2-Carbon/Images/adidas-Yeezy-Boost-350-V2-Carbon/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1603481985&w=1000"
-            alt="shoes1"
+            alt=""
         />
         <img className={styles.imageCarousel}
            src="https://images.unsplash.com/photo-1591550936261-4f22e48d1f03?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-             // src="https://stockx-360.imgix.net//Air-Jordan-1-Mid-Chicago-Toe/Images/Air-Jordan-1-Mid-Chicago-Toe/Lv2/img02.jpg?auto=format,compress&q=90&updated_at=1603481985&w=1000"
-             alt="shoes2"
+            alt=""
         />
         <img   className={styles.imageCarousel}
             src="https://images.unsplash.com/photo-1514989771522-458c9b6c035a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-             // src="https://stockx-360.imgix.net//Nike-Air-Force-1-Low-Supreme-Box-Logo-White/Images/Nike-Air-Force-1-Low-Supreme-Box-Logo-White/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1603481985&w=1000"
-             alt="shoes3"
+            alt=""
         />
         <img className={styles.imageCarousel}
             src="https://images.unsplash.com/photo-1580977251970-221b2fa3f554?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1778&q=80"
-             // src="https://stockx-360.imgix.net//Nike-Air-Rubber-Dunk-Off-White-UNC/Images/Nike-Air-Rubber-Dunk-Off-White-UNC/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1603756119&w=1000"
-             alt="shoes4"
+            alt=""
         />
         <img className={styles.imageCarousel}
             src="https://images.unsplash.com/photo-1581327512036-94637d8cdc2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1401&q=80"
-             // src="https://stockx-360.imgix.net//Air-Jordan-13-Retro-Flint-2020/Images/Air-Jordan-13-Retro-Flint-2020/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1603481985&w=1000"
-             alt="shoes5"
+            alt=""
         />
     </Carousel>
 );
